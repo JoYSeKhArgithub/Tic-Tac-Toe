3fb236f4477452c2a259c05004cf9753c97519c5
Joysekhar Banerjee <115169630+JoYSeKhArgithub@users.noreply.github.com>
2024-02-15 22:09:19 +0530

    Add files via upload

---
+++ Tic Tac Toe/script.js
@@ -0,0 +1,14 @@
+console.log("Welcome to Tic Tac Toe")
+let music = new Audio("music.mp3")
+let turn = new Audio("ting.mp3")
+let gameover = new Audio("gameover.mp3")
+
+
+const changeTurn =()=>{
+    return turn ==="X"?"0":"X";
+}
+
+const checkWin=()=>{
+
+}
+
